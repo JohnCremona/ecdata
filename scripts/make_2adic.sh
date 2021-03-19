b11eae8ba7c6326abaccefa8120c544dc473c594
John Cremona <john.cremona@gmail.com>
2021-03-19 11:16:28 +0000

    fix make_2adic script

--- scripts/make_2adic.sh
+++ scripts/make_2adic.sh
@@ -15,9 +15,9 @@ echo 'running Magma 2-adic script on '${r} in ${d}
 # - in allcurves files (as output by process_raw_curves): 1,2,4,6
 
 # for old-style allcurves file
-awk '{print $1,$2,$3,$4;}' ${d}/allcurves/allcurves.${r} > temp.${r}
+#awk '{print $1,$2,$3,$4;}' ${d}/allcurves/allcurves.${r} > temp.${r}
 # for new-style allcurves file
-#awk '{print $1,$2,$4,$6;}' ${d}/allcurves/allcurves.${r} > temp.${r}
+awk '{print $1,$2,$4,$6;}' ${d}/allcurves/allcurves.${r} > temp.${r}
 
 magma -b filename:=temp.${r} $HOME/ecdata/scripts/2adic.m
 mv 2adic.${r} ${d}/2adic/
